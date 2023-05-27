7190387b51f063ce240070faf402297a315876b2
Karim Altir <mhdkarim48850@gmail.com>
2023-05-27 16:40:46 +0200

    added posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,7 +7,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    hier komen alle posten
+                    @foreach ($posts as $post)
+                        <br>
+                        <h2>{{ $post->title }}</h2><br>
+                        <p>{{ $post->message }}</p>
+                    @endforeach
                 </div>
             </div>
         </div>
