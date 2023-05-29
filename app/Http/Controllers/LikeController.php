230df88686c87ddc9875b5e10d66f9def9a33eff
Karim Altir <mhdkarim48850@gmail.com>
2023-05-29 16:23:29 +0200

    added function into the likeController

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Like;
+use App\Models\Post;
+use Auth;
 
-class LikeController extends Controller
-{
-    //
+class LikeController extends Controller {
+    //de gebruiker die uitgelogd is kan geen like geven aan de posts.
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    public function like($postid, Request $request) {
+        
+        $post = Post::findOrFail($postid);
+        if($post->user_id == Auth::user()->id) {
+            abort(403, 'Cannot like own post');
+        }
+
+        $like = Like::where('post_id', '=', $postid)->where('user_id', '=', Auth::user()->id)->first();
+
+        if($like != NULL) {
+            abort(403,'Cannot like a post more than once');
+        }
+        
+        $like = new Like();
+        $like->user_id = Auth::user()->id;
+        $like->post_id = $postid;
+        $like->save();
+
+        return redirect()->route('index')->with('status', 'Post liked');
+    }
 }
