63ef57b573ff6ba7c5a3e00cd9c06b928495e191
Karim Altir <mhdkarim48850@gmail.com>
2023-05-29 20:06:38 +0200

    added class for header

--- resources/views/profile/partials/post.blade.php
+++ resources/views/profile/partials/post.blade.php
@@ -6,12 +6,12 @@
     </header>
     <form method="get" action="profiel/post" class="mt-6 space-y-6">
 
-        <h2>Gemaakte posts</h2>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Gemaakte posts</h2>
                 @foreach ($user->posts as $post)
                     <a href="">{{ $post->title }}</a><br>
                 @endforeach
                 <hr>
-        <h2>Gelikte posts</h2>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Gelikte posts</h2>
                 @foreach ($user->likes as $like)
                     <a href="">{{ $like->post->title }}</a><br>
                 @endforeach
