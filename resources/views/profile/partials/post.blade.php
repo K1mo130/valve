452a4d250075c080e418d2a0701e20d70e2a982d
Karim Altir <mhdkarim48850@gmail.com>
2023-05-29 19:52:39 +0200

    added header and posts + like list

---
+++ resources/views/profile/partials/post.blade.php
@@ -0,0 +1,20 @@
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ __('Recent Post') }}
+        </h2>
+    </header>
+    <form method="get" action="profiel/post" class="mt-6 space-y-6">
+
+        <h2>Gemaakte posts</h2>
+                @foreach ($user->posts as $post)
+                    <a href="">{{ $post->title }}</a><br>
+                @endforeach
+                <hr>
+        <h2>Gelikte posts</h2>
+                @foreach ($user->likes as $like)
+                    <a href="">{{ $like->post->title }}</a><br>
+                @endforeach
+                <hr>
+    </form>
+</section>
