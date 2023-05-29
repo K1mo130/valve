b1b9ee2e763b263f6a2a8479c4e939905d8833bd
Karim Altir <mhdkarim48850@gmail.com>
2023-05-29 12:46:06 +0200

    creating edit page for post

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,39 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Edit post</h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form method="POST" action="{{ route('posts.update', $post->id) }}">
+                        @csrf
+                        @method('PUT')
+
+                        <div>
+                            <label for="title">title:</label><br>
+                            <input name="title" type="text" value="{{ $post->title }}"><br>
+
+                            @error('title')
+                                <strong>{{ $message }}</strong>
+                            @enderror
+                        </div>
+
+                        <div>
+                            <label for="content">Content</label><br>
+                            <textarea name="content" cols="30" rows="10">{{ $post->message }}</textarea><br>
+
+                            @error('content')
+                                <strong>{{ $message }}</strong>
+                            @enderror
+                        </div>
+
+                        <button type="submit">Edit Post</button>
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
