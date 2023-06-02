229e9412c497ee3959acb4e4970b06fa5ceaeba8
Karim Altir <mhdkarim48850@gmail.com>
2023-06-02 14:34:38 +0200

    showing the message for logged in and for guest

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                @guest
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    {{ __("You have to log in or register") }}
                 </div>
+                @else
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Welcome, ") . Auth::user()->name }}
+                </div>
+                @endguest
             </div>
         </div>
     </div>
