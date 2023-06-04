20b9f4458f109c8857e9e7cedca5ca5d74f3e831
Karim Altir <mhdkarim48850@gmail.com>
2023-06-04 12:44:40 +0200

    change the name of the navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Home page') }}
                     </x-nav-link>
                     @auth
                         <x-nav-link :href="route('index')" :active="request()->routeIs('index')">
