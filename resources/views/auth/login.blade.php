370dacdf9471bd57d0675901b4ab385f26855057
Karim Altir <mhdkarim48850@gmail.com>
2023-06-13 15:26:58 +0200

    adding register link when the login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,9 +39,15 @@
                 </a>
             @endif
 
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+            <div class="ml-4">
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+                    {{ __('Register') }}
+                </a>
+    
+                <x-primary-button class="ml-3">
+                    {{ __('Log in') }}
+                </x-primary-button>
+            </div>
         </div>
     </form>
 </x-guest-layout>
