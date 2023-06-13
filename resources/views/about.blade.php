09109ea5dc851d4cfe2baa8480161ac1c20b662a
Karim Altir <mhdkarim48850@gmail.com>
2023-06-13 15:25:43 +0200

    adding content for the about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,30 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('About us') }}
+        <h2 class="h2_title">
+            {{ __('About') }}
         </h2>
     </x-slot>
 
     <div class="container_body">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    
+        <div class="container_main">
+            <div class="border">
+                <div class="border_text">
+                    <h3>About Us</h3>
+                    <p>
+                        In this project i just made the products by the API link for each product, You could add a product (or a game) in your Library, just like <strong>Steam</strong>.
+                    </p>
+                    <br>
+                    <h3>Resources Used:</h3><br>
+                    <ul>
+                        <li>
+                            Resource 1 - <a href="https://rawg.io/apidocs" target="_blank">The website <strong>Rawg.io</strong> to get the API link </a>
+                        </li>
+                        <br>
+                        <li>
+                            Resource 2 - <a href="https://api.rawg.io/api/games?key=5af1cfd2f6004e0698ee9fc217dd31fc&dates=2019-09-01,2019-09-30&platforms=18,1,7" target="_blank">The API itselfs</a>
+                        </li>
+                        <!-- Add more resources here -->
+                    </ul>
                 </div>
             </div>
         </div>
