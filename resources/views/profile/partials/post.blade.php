eaeb2b257db5a607d176eb7f017e117778c4a98f
Karim Altir <mhdkarim48850@gmail.com>
2023-06-17 13:35:36 +0200

    changing the text into English

--- resources/views/profile/partials/post.blade.php
+++ resources/views/profile/partials/post.blade.php
@@ -6,12 +6,12 @@
     </header>
     <form method="get" action="profiel/post" class="mt-6 space-y-6">
 
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Gemaakte posts</h2>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Created News</h2>
                 @foreach ($user->posts as $post)
                     <a href="{{ route('posts.show', $post->id) }}">{{ $post->title }}</a><br>
                 @endforeach
                 <hr>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Gelikte posts</h2>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Liked News</h2>
                 @foreach ($user->likes as $like)
                     <a href="{{ route('posts.show', $like->post_id) }}">{{ $like->post->title }}</a><br>
                 @endforeach
